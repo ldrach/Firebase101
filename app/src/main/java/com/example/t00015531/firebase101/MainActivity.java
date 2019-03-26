7c29667f75b521277ba7cfe2df1a63f145c0d9e8
ldrach <lyndondrach@gmail.com>
2019-03-26 15:11:34 -0700

    add read methods

--- app/src/main/java/com/example/t00015531/firebase101/MainActivity.java
+++ app/src/main/java/com/example/t00015531/firebase101/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.t00015531.firebase101;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +38,40 @@ public class MainActivity extends AppCompatActivity {
         myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        //allows input to the DB
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                myRef.setValue(s.toString());
+            }
+        });
+
+        //Reads from DB
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("TAG", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
